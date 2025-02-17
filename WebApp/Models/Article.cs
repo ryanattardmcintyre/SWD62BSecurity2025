365725068205a6b79c6066678b97c534a3d4788a
Ryan Attard <ryanattard@gmail.com>
2025-02-17 10:48:54 +0100

    Add project files.

---
+++ WebApp/Models/Article.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApp.Models
+{
+    public class Article
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [StringLength(100)]
+        [Required]
+        public string Title { get; set; }
+        public string Text { get; set; }
+
+        [ForeignKey("User")]
+        public string Author { get; set; }
+       
+        //navigational property will give us access to the IdentityUser (built-in class) additonal info
+        public virtual IdentityUser User { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ WebApp/Models/Artifact.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApp.Models
+{
+    public class Artifact
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; } 
+
+        public string FilePath { get; set; }
+
+        [ForeignKey("Article")]
+        public Guid ArticleIdFK { get; set; }
+
+        public virtual Article Article { get; set; }
+
+        //its a measure how can we detect that the article was tampered with
+        public string Digest { get; set; }
+
+    }
+}
